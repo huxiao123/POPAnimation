6acc025f3a61edce3095e3e0c700030161265f93
huxiao123 <382011377@qq.com>
2016-11-16 11:47:02 +0800

    Initial Commit

---
+++ POPAnimationDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  POPAnimationDemo
+//
+//  Created by 胡啸－ Mac on 16/11/16.
+//  Copyright © 2016年 gzsc-hx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
